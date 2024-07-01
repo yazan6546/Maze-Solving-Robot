20514c3c8381eeaeddca6a68251827c6b63a1425
yazan AbuAloun <1210145@student.birzeit.edu>
2024-07-02 01:03:55 +0300

    Added codes

---
+++ Encoder.cpp
@@ -0,0 +1,94 @@
+#include "Encoder.h"
+
+// Global pointers initialization
+Encoder *encoderInstanceA = nullptr;
+Encoder *encoderInstanceB = nullptr;
+
+Encoder::Encoder(uint8_t pinA, uint8_t pinB)
+    : pinA(pinA), pinB(pinB), encoderCount(0) {
+    // Determine the interrupt numbers for the pins
+    interruptA = digitalPinToInterrupt(pinA);
+    interruptB = digitalPinToInterrupt(pinB);
+
+    // Assign global pointers based on pin assignments
+    if (pinA == 35 && pinB == 32) {
+        encoderInstanceA = this;
+    } else if (pinA == 17 && pinB == 16) {
+        encoderInstanceB = this;
+    }
+}
+
+
+void Encoder::begin() {
+    pinMode(pinA, INPUT);
+    pinMode(pinB, INPUT);
+
+    this->oldPosition = 0;
+    this->lastTime = millis();
+
+    // Attach interrupts using global pointers and ISR functions
+    if (encoderInstanceA == this) {
+        attachInterrupt(interruptA, handleInterrupt_channe1A, RISING);
+        attachInterrupt(interruptA, handleInterrupt_channe2A, RISING);
+    } else if (encoderInstanceB == this) {
+        attachInterrupt(interruptB, handleInterrupt_channe1B, RISING);
+        attachInterrupt(interruptB, handleInterrupt_channe2B, RISING);
+    }
+}
+
+long Encoder::getCount() const {
+    return encoderCount;
+}
+
+void Encoder::resetCount() {
+    encoderCount = 0;
+}
+
+// Actual ISR code for pin A
+void handleInterrupt_channe1A() {
+    if (encoderInstanceA != nullptr) {
+        encoderInstanceA->handleInterrupt1_inst();
+    }
+}
+
+// Actual ISR code for pin A
+void handleInterrupt_channe2A() {
+    if (encoderInstanceA != nullptr) {
+        encoderInstanceA->handleInterrupt2_inst();
+    }
+}
+
+
+// Actual ISR code for pin B
+void handleInterrupt_channe1B() {
+    if (encoderInstanceB != nullptr) {
+        encoderInstanceB->handleInterrupt2_inst();
+    }
+}
+
+// Actual ISR code for pin B
+void handleInterrupt_channe2B() {
+    if (encoderInstanceB != nullptr) {
+        encoderInstanceB->handleInterrupt1_inst();
+    }
+}
+
+// Non-static member function for handling pin A interrupt
+void Encoder::handleInterrupt1_inst() {
+    // Handle encoder count for pin A
+    if (digitalRead(this->pinA)) {
+        this->encoderCount++;
+    } else if (digitalRead(this->pinB)) {
+        this->encoderCount--;
+    }
+}
+
+// Non-static member function for handling pin B interrupt
+void Encoder::handleInterrupt2_inst() {
+    // Handle encoder count for pin B
+    if (digitalRead(this->pinB)) {
+        this->encoderCount--;
+    } else if (digitalRead(this->pinA)) {
+        this->encoderCount++;
+    }
+}
---
+++ Encoder.h
@@ -0,0 +1,34 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include <Arduino.h>
+
+class Encoder {
+public:
+    Encoder(uint8_t pinA, uint8_t pinB);
+    void begin();
+    long getCount() const;
+    void resetCount();
+    void handleInterrupt1_inst();
+    void handleInterrupt2_inst();
+    long oldPosition;
+    unsigned long lastTime;
+
+private:
+    volatile long encoderCount;
+    uint8_t pinA;
+    uint8_t pinB;
+    uint8_t interruptA;
+    uint8_t interruptB;
+};
+
+// Global pointers to Encoder instances
+extern Encoder *encoderInstanceA;
+extern Encoder *encoderInstanceB;
+void handleInterrupt_channe1A();
+void handleInterrupt_channe2A();
+void handleInterrupt_channe1B();
+void handleInterrupt_channe2B();
+// Interrupt handler functions
+
+#endif // ENCODER_H
